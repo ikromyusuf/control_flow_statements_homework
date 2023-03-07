1ad3557b608c6f3c5d0d02b2bec8caa807b5ab85
Ikrom Yusupov <81753571+ikromyusuf@users.noreply.github.com>
2023-03-07 17:22:36 +0500

    Update if09.py

--- if09.py
+++ if09.py
@@ -1,4 +1,4 @@
-def main(a):
+def main(a=34):
     """
     The two-digit integer is given.
     Replace the digits of the number.
@@ -9,4 +9,4 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    return (a//10+a%10*10)<a
